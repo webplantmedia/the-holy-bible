b1a7a7b0d9f72d4767e2caa1191e4108414ba32e
Chris Baldelomar <chris@webplantmedia.com>
2017-12-30 15:03:31 -0600

    updated utf8

--- functions.php
+++ functions.php
@@ -55,7 +55,7 @@ function get_html_header() {
 	<?php ob_start(); ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 	<html xmlns="http://www.w3.org/1999/xhtml">
 		<head>
-			<meta charset="UTF-8">
+			<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
 			<title><?php echo $translation['title']; ?></title>
 			<link rel="stylesheet" type="text/css" href="<?php echo $translation['stylesheet']; ?>" />
 		</head>
